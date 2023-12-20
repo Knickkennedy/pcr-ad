13b40805381b4233c7a24cdd762e547674d9a46b
Pete Nuwayser <pnuw@ibm.com>
2023-12-20 14:03:29 -0500

    initial

---
+++ column2.py
@@ -0,0 +1,6 @@
+# Column 2: Count of Observed 30-day Readmissions (numerator)
+    # Count the number of observed IHS among nonoutlier beneficiaries with a readmission 
+    # within 30 days of discharge and enter this value into the reporting table under 
+    # Count of Observed 30-Day Readmissions (Table PCR-A, column 2).
+
+    # Step 1
